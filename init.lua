c31e3ad667827ff322e0cce5186213f89440c88f
Andres Fernandez <jandresfgz@gmail.com>
2025-04-02 11:58:03 -0500

    feat: add keymaps for buffer next/prev + inst dropbar for breadcrumbs

--- init.lua
+++ init.lua
@@ -568,9 +568,25 @@ require('lazy').setup({
       -- …etc.
     },
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    vim.keymap.set('n', '<leader><Right>', '<cmd>BufferNext<cr>', { desc = 'Next Buffer' }),
+    vim.keymap.set('n', '<leader><Left>', '<cmd>BufferPrevious<cr>', { desc = 'Previous Buffer' }),
   },
   {
     'vimlab/split-term.vim',
+  },
+  {
+    'Bekaboo/dropbar.nvim',
+    -- optional, but required for fuzzy finder support
+    dependencies = {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+    },
+    config = function()
+      local dropbar_api = require 'dropbar.api'
+      vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+      vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+      vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+    end,
   },
 
   -- LSP Plugins
