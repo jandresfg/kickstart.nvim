8b6016abdd53a0814f276fd77afc4269fd69df52
Andres Fernandez <jandresfgz@gmail.com>
2025-04-02 11:56:30 -0500

    fix: set scrolloff to zero

--- init.lua
+++ init.lua
@@ -157,7 +157,7 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 0
 
 -- commands for fold
 vim.opt.foldcolumn = '4'
