e2e8746f3d27cef45d8e67431dfb5de5f8daa365
Andres Fernandez <jandresfgz@gmail.com>
2025-04-08 09:53:44 -0500

    feat: tune tiny-inline-diagnostic

--- init.lua
+++ init.lua
@@ -615,7 +615,16 @@ require('lazy').setup({
     event = 'VeryLazy', -- Or `LspAttach`
     priority = 1000, -- needs to be loaded in first
     config = function()
-      require('tiny-inline-diagnostic').setup()
+      require('tiny-inline-diagnostic').setup {
+        preset = 'powerline',
+        options = {
+          show_source = true,
+          multilines = {
+            enabled = true,
+            always_show = true,
+          },
+        },
+      }
       vim.diagnostic.config { virtual_text = false } -- Only if needed in your configuration, if you already have native LSP diagnostics
     end,
   },
