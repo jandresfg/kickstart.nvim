ba6d265377b0920630012048895cee6874881085
Andres Fernandez <jandresfgz@gmail.com>
2025-10-24 17:09:39 -0500

    feat: add keymap for yanking filename

--- init.lua
+++ init.lua
@@ -196,6 +196,13 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Yank filename
+vim.keymap.set('n', '<leader>y', function()
+  local filename = vim.fn.expand '%:t'
+  vim.fn.setreg('+', filename)
+  vim.notify('Yanked filename: ' .. filename)
+end, { desc = '[Y]ank filename' })
+
 -- NOTE: Some terminals have coliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
