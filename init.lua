6779bc6cfb261e34b4a2e47d3e9e88e9943193ae
Andres Fernandez <jandresfgz@gmail.com>
2026-02-19 10:18:01 -0500

    added gitlab.nvim plugin + lazy-lock update

--- init.lua
+++ init.lua
@@ -760,6 +760,23 @@ require('lazy').setup({
       require('blame').setup {}
     end,
   },
+  {
+    'harrisoncramer/gitlab.nvim',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-lua/plenary.nvim',
+      'sindrets/diffview.nvim',
+      'stevearc/dressing.nvim', -- Recommended but not required. Better UI for pickers.
+      'nvim-tree/nvim-web-devicons', -- Recommended but not required. Icons in discussion tree.
+    },
+    build = function()
+      require('gitlab.server').build(true)
+    end, -- Builds the Go binary
+    config = function()
+      require('gitlab').setup()
+    end,
+  },
+  -- HERE'S THE END OF MY ADDED PLUGINS
 
   -- LSP Plugins
   {
