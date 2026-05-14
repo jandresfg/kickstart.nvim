8e508fe3bd1310ce71f7b83c3a0fce06b551a12a
Andres Fernandez <jandresfgz@gmail.com>
2026-05-14 10:38:11 -0500

    feat: nvim-ufo looks better now

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -4,19 +4,20 @@ return {
     dependencies = 'kevinhwang91/promise-async',
     event = 'BufRead',
     config = function()
-      vim.o.foldcolumn = '1'
+      vim.o.foldcolumn = 'auto:9'
       vim.o.foldlevel = 99
       vim.o.foldlevelstart = 99
       vim.o.foldenable = true
+      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
       vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
       vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 
-      require('ufo').setup({
+      require('ufo').setup {
         provider_selector = function(bufnr, filetype, buftype)
           return { 'treesitter', 'indent' }
         end,
-      })
+      }
     end,
   },
 }
