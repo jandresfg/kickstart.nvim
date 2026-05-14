983f5bd37197437c932a25821a5bebd85514d80a
Andres Fernandez <jandresfgz@gmail.com>
2026-05-14 10:07:37 -0500

    chore: make nvim-ufo config replicate minimal config from docs

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -6,16 +6,17 @@ return {
     config = function()
       vim.o.foldcolumn = '1'
       vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
       vim.o.foldenable = true
-
-      require('ufo').setup {
-        provider_selector = function(bufnr, filetype, buftype)
-          return { 'treesitter', 'indent' }
-        end,
-      }
 
       vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
       vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+      require('ufo').setup({
+        provider_selector = function(bufnr, filetype, buftype)
+          return { 'treesitter', 'indent' }
+        end,
+      })
     end,
   },
 }
